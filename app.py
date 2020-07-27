e16e19c93fb623063b1e4366c4783d534d9f35fd
yichen97 <yichen97.dai@gmail.com>
2020-07-27 22:11:29 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,24 +1,80 @@
 from flask import Flask, render_template
 from flask import url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+import click
+
+
+WIN = sys.platform.startswith('win')
+if WIN: 
+    prefix = 'sqlite:///'
+else :
+    prefix = 'sqlite:////'
 
 app = Flask(__name__)
 
-name = 'yision'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poers Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1996'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
+
+
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
+
+class User(db.Model):  #  表名将会是 user（自动生成， 小写处理）
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):  #b 表明将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  #电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+
+
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop')
+    # 设置选项
+def initdb(drop):
+    # Initialize the database
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+@app.cli.command()
+def forge():
+    # Generate fake data.
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'yision'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poers Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1996'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
     ]
+    user = User(name = name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title = m['title'], year = m['year'])
+        db.session.add(movie)
+
+    db.session.commit()  
+    click.echo('Done.')
+
 @app.route('/')
 def index():
-    return render_template('index.html', name = name, movies =movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user = user, movies =movies)
 
 @app.route('/user/<name>')
 # <name>可以匹配关键字
